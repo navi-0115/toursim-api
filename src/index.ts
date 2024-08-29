c369e22f783d1555fa5bc5ca04f473bd15b198bd
Raden-nvii <navinugraha15@gmail.com>
2024-08-29 22:56:57 +0800

    feat: add GET /places method

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,38 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { SwaggerUI } from "@hono/swagger-ui";
+import { PrismaClient } from "@prisma/client";
 
-const app = new Hono()
+const app = new Hono();
+const prisma = new PrismaClient();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+// Use the middleware to serve Swagger UI at /ui
+app.get("/ui", (c) => {
+  return c.html(`
+    <html lang="en">
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Custom Swagger" />
+        <title>Custom Swagger</title>
+        <script>
+          // custom script
+        </script>
+        <style>
+          /* custom style */
+        </style>
+      </head>
+      ${SwaggerUI({ url: "/doc" })}
+    </html>
+  `);
+});
 
-export default app
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/places", async (c) => {
+  const places = await prisma.place.findMany();
+  return c.json(places);
+});
+
+export default app;
